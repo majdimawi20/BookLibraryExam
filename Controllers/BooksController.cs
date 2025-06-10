ca43cab16337d274d31bb677c6cfb11088788140
majdimawi20 <majdimawi@gmail.com>
2025-06-10 03:09:55 -0700

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BookLibraryExam.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookLibraryExam.Controllers
+{
+    public class BooksController : Controller
+    {
+
+        private static List<Book> books = new List<Book>
+        {
+            new Book { Id = 1, Title = "Book One", Author = "Author A", ISBN = "1234567890123", PublicationYear = 2020 },
+            new Book { Id = 2, Title = "Book Two", Author = "Author B", ISBN = "9876543210987", PublicationYear = 2018 }
+        };
+
+        // GET: /Books
+        public IActionResult Index()
+        {
+            return View(books);
+        }
+
+        // POST: /Books/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Book newBook)
+        {
+            if (!ModelState.IsValid)
+            {
+
+                return View("Index", books);
+            }
+
+
+            int newId = books.Any() ? books.Max(b => b.Id) + 1 : 1;
+            newBook.Id = newId;
+
+            books.Add(newBook);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: /Books/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null) return NotFound();
+
+            return View(book);
+        }
+
+        // POST: /Books/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Book editedBook)
+        {
+            if (id != editedBook.Id)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return View(editedBook);
+
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null) return NotFound();
+
+            book.Title = editedBook.Title;
+            book.Author = editedBook.Author;
+            book.ISBN = editedBook.ISBN;
+            book.PublicationYear = editedBook.PublicationYear;
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: /Books/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null) return NotFound();
+
+            books.Remove(book);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookLibraryExam.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, ErrorMessage = "Title can't be longer than 100 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Author is required")]
+        [StringLength(50, ErrorMessage = "Author can't be longer than 50 characters")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "ISBN is required")]
+        [RegularExpression(@"\d{13}", ErrorMessage = "ISBN must be exactly 13 digits")]
+        public string ISBN { get; set; }
+
+        [Range(1500, 2100, ErrorMessage = "Publication year must be between 1500 and 2100")]
+        public int PublicationYear { get; set; }
+    }
+}
