211f83a517d2be02278e8915cf19042e68f22cb6
majdimawi20 <majdimawi@gmail.com>
2025-06-10 05:12:13 -0700

    edit

--- Models/Book.cs
+++ Models/Book.cs
@@ -4,21 +4,22 @@ namespace BookLibraryExam.Models
 {
     public class Book
     {
-        public int Id { get; set; }
+        public class Book
+        {
+            public int Id { get; set; }
 
-        [Required(ErrorMessage = "Title is required")]
-        [StringLength(100, ErrorMessage = "Title can't be longer than 100 characters")]
-        public string Title { get; set; }
+            [Required(ErrorMessage = "Title is required")]
+            public string Title { get; set; }
 
-        [Required(ErrorMessage = "Author is required")]
-        [StringLength(50, ErrorMessage = "Author can't be longer than 50 characters")]
-        public string Author { get; set; }
+            [Required(ErrorMessage = "Author is required")]
+            public string Author { get; set; }
 
-        [Required(ErrorMessage = "ISBN is required")]
-        [RegularExpression(@"\d{13}", ErrorMessage = "ISBN must be exactly 13 digits")]
-        public string ISBN { get; set; }
+            [Required(ErrorMessage = "ISBN is required")]
+            [RegularExpression(@"^\d{10}(\d{3})?$", ErrorMessage = "Invalid ISBN format")]
+            public string ISBN { get; set; }
 
-        [Range(1500, 2100, ErrorMessage = "Publication year must be between 1500 and 2100")]
-        public int PublicationYear { get; set; }
+            [Required(ErrorMessage = "Publication year is required")]
+            [Range(1000, 9999, ErrorMessage = "Year must be between 1000 and 9999")]
+            public int PublicationYear { get; set; }
+        }
     }
-}
